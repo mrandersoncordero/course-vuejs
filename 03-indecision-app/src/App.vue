e94712d39d8e2e0ada9317f094b99810eca2f37d
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-17 15:34:09 -0400

    componente tradicional

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -2,9 +2,10 @@
   <h1>Mi primera app</h1>
   <hr />
 
-  <MyCounter />
+  <MyCounterScript :value="10" />
 </template>
 
 <script lang="ts" setup>
 import MyCounter from './components/MyCounter.vue';
+import MyCounterScript from './components/MyCounterScript.vue';
 </script>
---
+++ 03-indecision-app/src/components/MyCounterScript.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <h3>Counter: {{ counter }}</h3>
+    <h3>Square: {{ squareCounter }}</h3>
+  </section>
+
+  <div>
+    <button @click="counter++">+1</button>
+    <button @click="counter--">-1</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref, computed } from 'vue';
+
+export default defineComponent({
+  props: {
+    value: { type: Number, required: true },
+  },
+  setup(props) {
+    const counter = ref(props.value);
+    const squareCounter = computed(() => counter.value * counter.value);
+
+    return {
+      counter,
+      squareCounter,
+    };
+  },
+});
+// import { ref, computed } from 'vue';
+
+// interface Props {
+//   value?: number;
+// }
+
+// const props = defineProps<Props>();
+
+// const counter = ref(props.value ?? 5);
+// const squareCounter = computed(() => counter.value * counter.value);
+</script>
