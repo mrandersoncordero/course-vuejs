e90ba1392186222924e9255ea4b5c3edab35c989
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-19 11:41:29 -0400

    usar composable functions en las demas secciones

--- 03-indecision-app/src/composables/useCounter.ts
+++ 03-indecision-app/src/composables/useCounter.ts
@@ -1,5 +1,6 @@
 import { computed, ref } from "vue";
 
+// const counter = ref(10);
 export const useCounter = (initialValue: number = 5) => {
   const counter = ref(initialValue);
   const squareCounter = computed(() => counter.value * counter.value);
