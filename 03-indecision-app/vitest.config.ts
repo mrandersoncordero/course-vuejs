75e9b778607aa58283cb40d3a99fc4087fcacea0
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-20 16:28:31 -0400

    configurar test

---
+++ 03-indecision-app/vitest.config.ts
@@ -0,0 +1,13 @@
+/// <reference types="vitest/config" />
+import { defineConfig, mergeConfig } from 'vite';
+import viteConfig from './vite.config';
+
+export default mergeConfig(
+  viteConfig,
+  defineConfig({
+    test: {
+      environment: 'jsdom',
+      globals: true,
+    },
+  }),
+);
