b544c07dfcce5d2691819a8df8ed6d25cb71e43b
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-20 16:28:44 -0400

    probando test de components

--- 03-indecision-app/src/components/MyCounter.vue
+++ 03-indecision-app/src/components/MyCounter.vue
@@ -1,7 +1,7 @@
 <template>
   <section>
     <h3>Counter: {{ counter }}</h3>
-    <h3>Square: {{ squareCounter }}</h3>
+    <h3 data-testid="square-label">Square: {{ squareCounter }}</h3>
   </section>
 
   <div>
@@ -13,7 +13,13 @@
 <script lang="ts" setup>
 import { useCounter } from '@/composables/useCounter';
 
-const { counter, squareCounter} = useCounter(10);
+interface Props {
+  value: number;
+}
+
+const props = defineProps<Props>();
+
+const { counter, squareCounter } = useCounter(props.value);
 // import { ref, computed } from 'vue';
 
 // interface Props {
@@ -21,8 +27,6 @@ const { counter, squareCounter} = useCounter(10);
 // }
 
 // const props = defineProps<Props>();
-
-
 </script>
 
 <style scoped>
---
+++ 03-indecision-app/tests/components/MyCounter.test.ts
@@ -0,0 +1,35 @@
+import { describe, expect, test } from 'vitest';
+import { mount } from '@vue/test-utils';
+import MyCounter from '@/components/MyCounter.vue';
+
+describe('<MyCounter />', () => {
+  test('should match snapshot', () => {
+    const wrapper = mount(MyCounter, {
+      props: {
+        value: 5,
+      },
+    });
+
+    expect(wrapper.html()).toMatchSnapshot();
+  });
+
+  test('renders the conunter value correctly', () => {
+    const value = 5;
+    const wrapper = mount(MyCounter, {
+      props: {
+        value: 5,
+      },
+    });
+
+    const [counterLabel, squareLabel] = wrapper.findAll('h3');
+
+    // expect(wrapper.find('h3').text()).toContain(`Counter: ${value}`);
+    // expect(wrapper.find('[data-testid="square-label"]').text()).toContain(
+    //   `Square: ${value * value}`,
+    // );
+
+    expect(counterLabel.text()).toContain(`Counter: ${value}`);
+    expect(squareLabel.text()).toContain(`Square: ${value * value}`);
+    // console.log(wrapper.find('h3').text());
+  });
+});
