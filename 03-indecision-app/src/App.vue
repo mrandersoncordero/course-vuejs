0ad8840f7d84c5e47f71a61602e9df48793bf64b
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-19 22:14:26 -0400

    iniciando proyecto de chat

--- 03-indecision-app/src/App.vue
+++ 03-indecision-app/src/App.vue
@@ -1,14 +1,7 @@
 <template>
-  <h1 class="text-3xl font-bold underline">Hola, mundo!</h1>
-  <hr />
-
-  <MyCounter />
-  <MyCounterScript :value="2" />
-  <MyCounterScript2 :value="3" />
+  <IndecisionView/>
 </template>
 
 <script lang="ts" setup>
-import MyCounter from './components/MyCounter.vue';
-import MyCounterScript from './components/MyCounterScript.vue';
-import MyCounterScript2 from './components/my-counter-script/MyCounterScript2.vue';
+import IndecisionView from '@/views/IndecisionView.vue';
 </script>
---
+++ 03-indecision-app/src/views/IndecisionView.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="bg-gray-100 h-screen flex flex-col max-w-lg mx-auto">
+    <div class="bg-blue-500 p-4 text-white flex justify-between items-center">
+      <span>Mi esposa</span>
+    </div>
+
+    <!-- Chat Messages -->
+    <ChatMessages :messages="messages" />
+
+    <!-- MessageBox -->
+    <MessageBox />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import ChatMessages from '@/chat/ChatMessages.vue';
+import MessageBox from '@/chat/MessageBox.vue';
+import type { ChatMessage } from '@/interfaces/chat-messages.interface';
+import { ref } from 'vue';
+
+const messages = ref<ChatMessage[]>([
+  {
+    id: new Date().getTime(),
+    message: 'Hola mundo',
+    itsMine: true,
+  },
+  {
+    id: new Date().getTime() + 1,
+    message: 'Quieres ir a tomar cafe?',
+    itsMine: true,
+  },
+  {
+    id: new Date().getTime() + 2,
+    message: 'No',
+    itsMine: false,
+    image: 'https://yesno.wtf/assets/no/10-d5ddf3f82134e781c1175614c0d2bab2.gif',
+  },
+]);
+</script>
