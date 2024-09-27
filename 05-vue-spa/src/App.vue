0b2e1a7d16dcc031587781131d91904aed38575f
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-27 09:59:17 -0400

    vue router

---
+++ 05-vue-spa/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <a href="#/">Home</a>
+    <a href="#/features">Features</a>
+    <a href="#/pricing">Pricing</a>
+    <a href="#/contact">Contact</a>
+  </nav>
+  <main class="bg-gray-100">
+    <RouterView />
+  </main>
+</template>
+
+<script lang="ts" setup>
+import { RouterView } from 'vue-router';
+import HomePage from './modules/landing/pages/HomePage.vue';
+</script>
---
+++ 05-vue-spa/src/router/index.ts
@@ -0,0 +1,30 @@
+import HomePage from '@/modules/landing/pages/HomePage.vue';
+import { createRouter, createWebHashHistory } from 'vue-router';
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/', // ruta
+      name: 'home', // nombre de la ruta para poder acceder mas facilmente
+      component: HomePage, // el componente que se cargara en la vista
+    },
+    {
+      path: '/features',
+      name: 'features',
+      component: () => import('@/modules/landing/pages/FeaturesPage.vue'),
+    },
+    {
+      path: '/pricing',
+      name: 'pricing',
+      component: () => import('@/modules/landing/pages/PricingPage.vue'),
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: () => import('@/modules/landing/pages/ContactPage.vue'),
+    },
+  ],
+});
+
+export default router;
