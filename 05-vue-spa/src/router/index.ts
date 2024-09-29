a68fbba9297862e96f31826b16d107de588c9979
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-29 10:57:55 -0400

    las vistas heredan de los  layout

--- 05-vue-spa/src/router/index.ts
+++ 05-vue-spa/src/router/index.ts
@@ -5,24 +5,50 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/', // ruta
-      name: 'home', // nombre de la ruta para poder acceder mas facilmente
-      component: HomePage, // el componente que se cargara en la vista
+      path: '/',
+      name: 'landing',
+      component: () => import('@/modules/landing/layouts/LandingLayout.vue'),
+      children: [
+        {
+          path: '/', // ruta
+          name: 'home', // nombre de la ruta para poder acceder mas facilmente
+          component: HomePage, // el componente que se cargara en la vista
+        },
+        {
+          path: '/features',
+          name: 'features',
+          component: () => import('@/modules/landing/pages/FeaturesPage.vue'),
+        },
+        {
+          path: '/pricing',
+          name: 'pricing',
+          component: () => import('@/modules/landing/pages/PricingPage.vue'),
+        },
+        {
+          path: '/contact',
+          name: 'contact',
+          component: () => import('@/modules/landing/pages/ContactPage.vue'),
+        },
+      ],
     },
+
+    // Auth
     {
-      path: '/features',
-      name: 'features',
-      component: () => import('@/modules/landing/pages/FeaturesPage.vue'),
-    },
-    {
-      path: '/pricing',
-      name: 'pricing',
-      component: () => import('@/modules/landing/pages/PricingPage.vue'),
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      component: () => import('@/modules/landing/pages/ContactPage.vue'),
+      path: '/auth',
+      redirect: { name: 'login' },
+      component: () => import('@/modules/auth/layouts/AuthLayout.vue'),
+      children: [
+        {
+          path: 'login', // si quitamos el /login la ruta cambiara a  /auth/login
+          name: 'login',
+          component: () => import('@/modules/auth/pages/LoginPage.vue'),
+        },
+        {
+          path: 'register', // si quitamos el /register la ruta cambiara a  /auth/register
+          name: 'register',
+          component: () => import('@/modules/auth/pages/RegisterPage.vue'),
+        },
+      ],
     },
   ],
 });
