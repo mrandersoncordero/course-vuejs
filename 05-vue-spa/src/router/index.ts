2981307c6ed6d6dcb7129fa5e6cd371abfdd746a
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-29 10:21:40 -0400

    usar createWebHistory

--- 05-vue-spa/src/router/index.ts
+++ 05-vue-spa/src/router/index.ts
@@ -1,8 +1,8 @@
 import HomePage from '@/modules/landing/pages/HomePage.vue';
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router';
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/', // ruta
