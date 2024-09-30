c1683badd31782d37845380373d83bc215fa6088
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-30 08:27:07 -0400

    template pokemons

---
+++ 05-vue-spa/src/modules/pokemons/pages/PokemonPage.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="flex flex-col">
+        <h1 class="text-3xl font-bold">Pokemon <small class="text-blue-500">#{{ id }}</small></h1>
+
+        <img 
+            :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ id }.svg`"
+            alt="Pokemon"
+            class="w-[200px] h-[200px] mt-5"
+        >
+        <RouterLink 
+            class="bg-blue-500 text-white p-2 rounded mt-5 text-center hover:bg-blue-700"
+            :to="{ name: 'pokemon', params: { id: id + 1}}"
+        >Siguiente</RouterLink>
+    </section>
+</template>
+
+<script lang="ts" setup>
+interface Props {
+    id: number
+}
+
+const props = defineProps<Props>();
+console.log(props);
+
+</script>
