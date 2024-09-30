ed3a4456a6f5eab9fd970ce78aa513635686ca68
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-09-30 16:22:41 -0400

    test pokemon page RouterLink

---
+++ 05-vue-spa/tests/modules/pokemons/pages/PokemonPage.test.ts
@@ -0,0 +1,32 @@
+import PokemonPage from "@/modules/pokemons/pages/PokemonPage.vue"
+import { mount, RouterLinkStub } from '@vue/test-utils';
+import { RouterLink } from "vue-router";
+
+describe('<PokemonPage />', () => {
+    const wrapper = mount(PokemonPage, {
+        props: {
+            id: 25,
+        },
+        global: {
+            stubs: {
+                // RouterLink: true,
+                RouterLink: RouterLinkStub
+            }
+        }
+    });
+
+    test('should render the component correctly', () => {
+        expect(wrapper.find('h1').exists()).toBe(true);
+        expect(wrapper.find('img').attributes('src')).toBe(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg`)
+        
+        
+        console.log(wrapper.html());
+    });
+
+    test('should redirect to the next pokemon', () => {
+        const link = wrapper.findComponent(RouterLinkStub);
+        
+        expect(link.props().to).toEqual({ name: 'pokemon', params: { id: 26 } })
+        console.log(link.props());
+    })
+})
