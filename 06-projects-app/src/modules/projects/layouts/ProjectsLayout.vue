92737e28c471cce65c79166b8848989a2fd68e0d
mrandersoncordero <acorderofigueroa7@gmail.com>
2024-10-06 13:58:49 -0400

    projects layout

---
+++ 06-projects-app/src/modules/projects/layouts/ProjectsLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex flex-col">
+    <!-- Top menu -->
+    <top-menu />
+    <main>
+      <!-- Side Menu -->
+      <side-menu />
+      <!-- RouterView -->
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import TopMenu from '@/modules/common/components/TopMenu.vue';
+import SideMenu from '../components/SideMenu.vue';
+</script>
